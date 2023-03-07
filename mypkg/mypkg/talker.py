ec2e59e5b8511a87cea57c6d32ccc0ca4de03e06
Riku Okada <misawa.parple@gmail.com>
2023-03-08 03:31:18 +0900

    add talker

---
+++ mypkg/mypkg/talker.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2023 RIku Okada
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+rclpy.init()
+pub = node.create_publisher(Int16, "countup", 10)
+n = 0
+node.create_timer(0.5, self.cb)
+                                            
+def cb():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
+    n += 1
+
+node = Node("talker")
+talker = Talker(node)
+rclpy.spin(node)
