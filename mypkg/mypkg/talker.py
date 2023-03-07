ade6afe3b60af969703c02ebd94cd589a2e58f96
Riku Okada <misawa.parple@gmail.com>
2023-03-08 03:35:30 +0900

    add talker

--- mypkg/mypkg/talker.py
+++ mypkg/mypkg/talker.py
@@ -6,6 +6,7 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 rclpy.init()
+node = Node("talker")
 pub = node.create_publisher(Int16, "countup", 10)
 n = 0
 node.create_timer(0.5, self.cb)
